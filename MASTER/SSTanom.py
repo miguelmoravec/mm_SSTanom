48ac49c4c00b3a38793e7a423441eeca5112f349
Miguel Moravec <miguel.moravec@noaa.gov>
2016-07-15 16:36:55 -0400

    Significant overhaul introduces running options, failure checks, and quieter running

--- MASTER/SSTanom.py
+++ MASTER/SSTanom.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 #Written by Miguel M. Moravec. For questions please email miguel.moravec@vanderbilt.edu
-#This script automatically generates plots of SST anomlies over the Pacific for the preceding 4 months
+#This script automatically generates plots of Pacific SST anomalies for the 4 months preceding a specified date
 #This script relies on the standard naming convention of SST NetCDF files in this directory: /archive/x1y/FMS/c3/CM2.1_ECDA/CM2.1R_ECDA_v3.1_1960_pfl_auto/gfdl.ncrc3-intel-prod-openmp/history/tmp/
 #This script also relies on the historical data located in this archived file: /archive/x1y/yxue/realtime/temp.clim.1981_2010.nc
 
 import subprocess
 import datetime
 import os
+import os.path
+import sys, getopt
 
 try:
 	import pyferret
@@ -15,30 +17,73 @@ except ImportError:
 	print "You must module load pyferret"
 	exit(1)   
 
-def mymain():    
+def mymain(argv):    
 
+	now = datetime.datetime.now()
+	today = ''
+	
+	#the following establishes the three options for the script
+
+	try:
+		opts, args = getopt.getopt(argv,"thd:",["input="])
+
+	except getopt.GetoptError:
+   		print "ERROR Invalid Syntax. See 'SSTanom.py -h'"
+		sys.exit(2)
+
+	for opt, arg in opts:
+ 		if opt == '-h': #help option
+        		print '\nThis script automatically generates plots of Pacific SST anomalies for the 4 months preceding a specified date \n'
+			print 'Options are as follows:'
+			print "'-h' launches this help text"
+			print "'-t' generates today's most recent plots"
+			print "'-d mmyyy' generates plots for months preceding a particular date i.e. '-d 072016' \n"
+			print 'This script relies on the standard naming convention of SST NetCDF files in this directory:'
+			print '/archive/x1y/FMS/c3/CM2.1_ECDA/CM2.1R_ECDA_v3.1_1960_pfl_auto/gfdl.ncrc3-intel-prod-openmp/history/tmp/ \n'
+			print 'This script also relies on the historical data located in this archived file:'
+			print '/archive/x1y/yxue/realtime/temp.clim.1981_2010.nc \n'
+			print 'Written by Miguel M. Moravec. For questions please email miguel.moravec@vanderbilt.edu \n'
+        		sys.exit()
+
+		elif opt == '-t':
+			#option automatically generates most recent plots
+			today = now.strftime('%m%Y')
+
+		elif opt in ('-d', '--input'):
+         		#option generates plots for specific date mmyyyy
+			today = arg			
+	
+	#reminds user to select an option
+	if today == '':
+		print 'ERROR must select an option'
+		print "'-t' generates plots for months preceding today's date"
+		print "'-d mmyyy' generates plots for months preceding a particular date i.e. '-d 072016'"
+		exit(1)
+
+	try:
+		date = datetime.datetime.strptime('25' + today, '%d%m%Y')
+	except ValueError:
+		print "ERROR Invalid Syntax. Arguments following '-d' should be formatted: mmyyyy"
+		exit(1)
+    
 	#the following sets file naming convention and time variables, used in generation of NetCDFs, plots, and file names
 
 	basedir = '/archive/x1y/FMS/c3/CM2.1_ECDA/CM2.1R_ECDA_v3.1_1960_pfl_auto/gfdl.ncrc3-intel-prod-openmp/history/tmp/'
 	filetail = "01.ocean_month.ensm.nc"
-
-	print 'Please answer the following question to plot SST anomalies over the Pacific for the last 4 months...'
-	today = raw_input("Enter desired end date (mmyyyy): ")
-    
-	date = datetime.datetime.strptime('25' + today, '%d%m%Y')
     
 	month = date.strftime('%m')
 	year = date.strftime('%Y')
 
 	#the following automates the pyferret plot generation and saves a png image file in the local directory	
 
-	print 'Generating plots for months preceeding', month,'/', year, '...'
+	print 'Generating plots for Pacific SST anomalies for months preceeding', month,'/', year, '...'
 
 	filename = 'tempa_latest4mon_' + month + '_' + year + '.png'
 
-	pyferret.start(quiet=True)
-	os.remove("ferret.jnl")
-    
+	if ( not pyferret.start(quiet=True, journal=False, unmapped=True) ):
+		print "ERROR. Pyferret start failed. Exiting . . ."
+		exit(1)
+
 	header()
 
 	count = 0
@@ -75,9 +120,11 @@ def mymain():
 	(errval, errmsg) = pyferret.run(cmd9)
 	(errval, errmsg) = pyferret.run(cmd10)
 
-	print 'The image file containing the SST anomoly plots for the 4 months preceeding ', month,'/', year, ' is located in the local directory (if data was available in archive) and is named: ', filename
-	print 'If no plots generated, please see script comments to find necessary input files.'
-
+	if os.path.exists(str(filename)):
+		print 'SUCCESS. The image file containing the SST anomaly plots for the 4 months preceeding ', month,'/', year, ' is located in the local directory and is named:', filename
+	else:
+		print "ERROR. No plots generated. Please ensure data files are located in their proper directories. See '-h'"
+		exit(1)
 def header():
 
 	#the following clears data from previously running pyferrets, establishes base parameters, and loads historical data
@@ -111,4 +158,4 @@ def body():
 	(errval, errmsg) = pyferret.run(com11)
 
 if __name__=="__main__":
-    mymain()
+    mymain(sys.argv[1:])
